3d37e6f7cdeb19977a617ead9e4fc224731f5f4b
laura-nesta <99044194+laura-nesta@users.noreply.github.com>
2023-01-09 23:01:10 +0100

    Finalisation page accueil (#1)
    * test dark & light
    
    * page accueil complète
    
    * finalisation page accueil
    
    Co-authored-by: laura-nesta <laura.nesta@outlook.com>

--- fonctions/fonctions.js
+++ fonctions/fonctions.js
@@ -1,25 +1,43 @@
+//Switcher thème dark et light 
+
+// let theme = "dark";
+const monStockage = sessionStorage;
+monStockage.setItem('theme', 'dark');
+
+document.addEventListener('DOMContentLoaded', () => {
+  const theme = monStockage.getItem('theme');
+  darkMode(theme)
+    console.log(theme);
+});
+
 document.querySelectorAll('#light-icon').forEach(item => {
-    item.addEventListener('click', darkMode);
     item.addEventListener('click', switchThemeLight);
+    item.addEventListener('click', darkMode);
   });
 
   document.querySelectorAll('#dark-icon').forEach(item => {
-    item.addEventListener('click', darkMode);
     item.addEventListener('click', switchThemeDark);
+    item.addEventListener('click', darkMode);
   });
 
-  function darkMode() {
+  function darkMode(theme) {
     let themeSwitchableElement = document.querySelectorAll('[data-theme]');
     let mode = this.dataset.mode;
-    themeSwitchableElement.forEach(el => el.dataset.theme = mode)
+    var theme = monStockage.getItem('theme');
+    // themeSwitchableElement.forEach(el => el.dataset.theme = mode)
+    themeSwitchableElement.forEach(el => el.dataset.theme = theme)
   }
 
   function switchThemeDark() {
     document.querySelector('#dark-icon').classList.add('active');
     document.querySelector('#light-icon').classList.remove('active');
+    monStockage.setItem('theme', 'dark');
+    // theme = "dark";
   }
 
   function switchThemeLight() {
     document.querySelector('#light-icon').classList.add('active');
     document.querySelector('#dark-icon').classList.remove('active');
+    monStockage.setItem('theme', 'light');
+    // theme = "light"
   }
